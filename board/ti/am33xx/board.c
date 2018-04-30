e6a76b0032868015d0b8a8a527229f1ebdf93167
p.sakthivel <p.sakthivel@globaledgesoft.com>
2018-04-30 16:06:26 +0530

    + Source Tree structure updated
    Signed-off-by: p.sakthivel <p.sakthivel@globaledgesoft.com>

--- board/ti/am33xx/board.c
+++ board/ti/am33xx/board.c
@@ -1,7 +1,4 @@
-#define writel(v, a) (*(volatile unsigned int *)(a) = (v))
-#define readl(a) (*(volatile unsigned int *)a)
-#define writeb(v, a) (*(volatile unsigned char *)(a) = (v))
-#define readb(a) (*(volatile unsigned char *)a)
+#include<asm/io.h>
 
 typedef unsigned int long ulong;
 
@@ -46,34 +43,6 @@ void set_uart_mux_conf(void)
     writel(0, CTRL_BASE + 2420);
 }
 
-void uart_init(void)
-{
-#include <serial/ns16550.h>
-        struct ns16550 *com_port = (struct ns16550 *)0x44e09000;
-        int baud_divisor = 26;
-
-        writeb(CONFIG_SYS_NS16550_IER, &com_port->ier);
-        writeb(0x7, &com_port->mdr1);   /* mode select reset TL16C750*/
-        writeb(UART_LCR_BKSE | UART_LCRVAL, (ulong)&com_port->lcr);
-        writeb(0, &com_port->dll);
-        writeb(0, &com_port->dlm);
-        writeb(UART_LCRVAL, &com_port->lcr);
-        writeb(UART_MCRVAL, &com_port->mcr);
-        writeb(UART_FCRVAL, &com_port->fcr);
-        writeb(UART_LCR_BKSE | UART_LCRVAL, &com_port->lcr);
-        writeb(baud_divisor & 0xff, &com_port->dll);
-        writeb((baud_divisor >> 8) & 0xff, &com_port->dlm);
-        writeb(UART_LCRVAL, &com_port->lcr);
-        writeb(0, &com_port->mdr1);
-	while(1) {
-	    while ((readb(&com_port->lsr) & UART_LSR_THRE) == 0)
-		; 
-            writeb('1', &com_port->thr);
-	    while ((readb(&com_port->lsr) & UART_LSR_THRE) == 0)
-		; 
-            writeb('2', &com_port->thr);
-	}
-}
 void early_system_init(void)
 {
         /*
@@ -88,7 +57,7 @@ void early_system_init(void)
     uart_soft_reset();
     uart_init();
 
-//    do_check_uart();
+    do_check_uart();
 
     return;
 }
