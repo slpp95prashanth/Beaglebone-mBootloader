dbb3966e639ee01bad9cb1afed5936087597effc
p.sakthivel <p.sakthivel@globaledgesoft.com>
2018-05-23 19:08:51 +0530

    + Added prototypes for lib/srting.c + inttostr added

--- include/lib/string.h
+++ include/lib/string.h
@@ -1,3 +1,7 @@
-int strcmp(char *, char *);
+int strcmp(const char *, const char *);
+int strlen(const char *);
+char *strncpy(char *, const char *, int);
+int inttostr(int, char *, int);
+
 void asm_memset(void *, char, int);
 void memset(char *, char, int);
