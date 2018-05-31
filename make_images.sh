9474d5cd7079f0a9688cbe3044a438a98446332d
p.sakthivel <p.sakthivel@globaledgesoft.com>
2018-05-31 10:48:15 +0530

    + Added .bss section in linker script + Added make_images.sh for boot through UART0

---
+++ make_images.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -x
+#set -e
+
+if [ ! -f u-boot-spl.bin ] ; then
+    exit
+fi
+tty1=`ps -eaf | grep minicom | grep -v color | grep -o pts/...`
+tty1=`echo $tty1 | head -n1 | cut -d " " -f1`
+echo $tty1
+
+sudo pkill minicom
+
+sx -vvv u-boot-spl.bin < /dev/ttyUSB0 > /dev/ttyUSB0
+
+if [ ! -z "$tty1" ] ; then
+    ./tools/a.out -n /dev/$tty1 "sudo minicom"
+fi
+
+exit
+if [ ! -d /media/$USER/boot ]; then
+    sudo mkdir /media/$USER/boot
+    sudo mount /dev/sdc1 /media/$USER/boot
+fi
+
+sudo cp MLO /media/$USER/boot/
+sudo umount /media/$USER/*
+
+if [ -d /media/$USER/boot ]; then
+    sudo rm -r /media/$USER/boot
+fi
+
+
+
+
+
+
+
